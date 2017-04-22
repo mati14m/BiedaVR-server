c5bbb66b343fabb791f243ab16b675aa32322fac
Mefju <mati13m@gmail.com>
2017-04-22 14:19:27 +0200

    sending udp

--- kinect1/Program.cs
+++ kinect1/Program.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -9,6 +12,9 @@ namespace kinect1
 {
     class Program
     {
+
+        UdpClient client; 
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
@@ -16,6 +22,7 @@ namespace kinect1
         static void Main()
         {
             Program p = new Program();
+            p.createUdpConnection();
             p.StartKinectST();
         }
 
@@ -31,7 +38,7 @@ namespace kinect1
             skeletonData = new Skeleton[kinect.SkeletonStream.FrameSkeletonArrayLength]; // Allocate ST data
 
             kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinect_SkeletonFrameReady); // Get Ready for Skeleton Ready Events
-
+            
             kinect.Start(); // Start Kinect sensor
             while (true) { };
             
@@ -46,11 +53,41 @@ namespace kinect1
                     skeletonFrame.CopySkeletonDataTo(this.skeletonData); // get the skeletal information in this frame
                 }
             }
+            
             foreach(var s in skeletonData)
             {
-                Console.WriteLine(s.Position.X + ", " + s.Position.Y + ", " + s.Position.Z );
+                Joint head = s.Joints[JointType.Head];
+                if(s.Position.X == 0 && s.Position.Y == 0 && s.Position.Z == 0)
+                {
+                    continue;
+                }
+                String msg = "";
+                var jointTypes = Enum.GetValues(typeof(JointType));
+                foreach (JointType type in jointTypes)
+                {
+                    msg = msg + s.Joints[type].Position.X + ";" + s.Joints[type].Position.Y + ";" + s.Joints[type].Position.Z + ";";
+                }
+                msg = msg.Replace(',', '.');
+                //Console.WriteLine(msg);
+                sendToVR(msg);
+                //Console.WriteLine(head.Position.X + ", " + head.Position.Y + ", " + head.Position.Z);
+                //Console.WriteLine(s.Position.X + ", " + s.Position.Y + ", " + s.Position.Z );
             }
             Console.WriteLine();
+        }
+
+        private void createUdpConnection()
+        {
+            client = new UdpClient();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("192.168.0.38"), 8082);
+            client.Connect(ep);
+        }
+
+        private void sendToVR(string text)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(text);
+            int numberOfBytes = text.Length;
+            client.Send(msg, numberOfBytes);
         }
 
     }
