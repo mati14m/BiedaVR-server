b34f2fa230a51db2eab5335821dd00795e57cb56
Mefju <mati13m@gmail.com>
2017-04-22 10:58:04 +0200

    init

---
+++ kinect1/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Kinect;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kinect1
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Program p = new Program();
+            p.StartKinectST();
+        }
+
+
+        KinectSensor kinect = null;
+        Skeleton [] skeletonData = null;
+
+        void StartKinectST()
+        {
+            kinect = KinectSensor.KinectSensors.FirstOrDefault(s => s.Status == KinectStatus.Connected); // Get first Kinect Sensor
+            kinect.SkeletonStream.Enable(); // Enable skeletal tracking
+
+            skeletonData = new Skeleton[kinect.SkeletonStream.FrameSkeletonArrayLength]; // Allocate ST data
+
+            kinect.SkeletonFrameReady += new EventHandler<SkeletonFrameReadyEventArgs>(kinect_SkeletonFrameReady); // Get Ready for Skeleton Ready Events
+
+            kinect.Start(); // Start Kinect sensor
+            while (true) { };
+            
+        }
+
+        private void kinect_SkeletonFrameReady(object sender, SkeletonFrameReadyEventArgs e)
+        {
+            using (SkeletonFrame skeletonFrame = e.OpenSkeletonFrame()) // Open the Skeleton frame
+            {
+                if (skeletonFrame != null && this.skeletonData != null) // check that a frame is available
+                {
+                    skeletonFrame.CopySkeletonDataTo(this.skeletonData); // get the skeletal information in this frame
+                }
+            }
+            foreach(var s in skeletonData)
+            {
+                Console.WriteLine(s.Position.X + ", " + s.Position.Y + ", " + s.Position.Z );
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
